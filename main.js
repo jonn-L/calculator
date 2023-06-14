e699b5b4a48c93ff54c7ea5447e7e1c1ec26a4e0
Jon Lumi <jlumi@constructor.university>
2023-06-14 12:32:59 +0200

    buid html skeleton, make basic functions for basic operations in js

---
+++ main.js
@@ -0,0 +1,41 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            console.log('error');
+
+    }
+}
+
+const display = document.querySelector('.display')
+const buttons_number = document.querySelectorAll('.number');
+buttons_number.forEach(button => button.addEventListener('click', function() {
+    display.textContent += button.id;
+}))
